fbc5cce6cfa13bd7852750bc4e6fe9c1ad160686
Lesha Tagil <lesha.tagil.65@gmail.com>
2025-04-05 09:36:05 +0200

    HW5. fixes8

---
+++ homeworks/hw7/bulls_and_cows/hw7_solution.py
@@ -0,0 +1,14 @@
+# import random
+# import string
+#
+#
+# def generate_secret_number() -> str:
+#     pass
+#
+#
+# def check_guess(guess: str, secret_number: str) -> tuple:
+#     pass
+#
+#
+# def bulls_and_cows() -> None:
+#     pass
