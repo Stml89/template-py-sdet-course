1289794dec5b3e8bc3c1b3d08f9f3dd9c0baf53d
Lesha Tagil <lesha.tagil.65@gmail.com>
2025-04-05 08:53:43 +0200

    HW5. fixes6

--- homeworks/hw6/hw6_solution.py
+++ homeworks/hw6/hw6_solution.py
@@ -1,10 +1,10 @@
-# def level_up(experience: int, threshold: int, reward: int) -> bool:
-#     pass
-#
-#
-# def motor_time(n: int) -> int:
-#     pass
-#
-#
-# def time_converter(time_str: str) -> str:
-#     pass
+def level_up(experience: int, threshold: int, reward: int) -> bool:
+    pass
+
+
+def motor_time(n: int) -> int:
+    pass
+
+
+def time_converter(time_str: str) -> str:
+    pass
